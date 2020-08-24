aa91f06d96378e363884a481427a1f2a67baf064
IsaacDev <isaac.montiels@hotmail.com>
2020-08-24 04:04:56 -0500

    Socket Cliente

--- src/Cliente/VentanaCliente.java
+++ src/Cliente/VentanaCliente.java
@@ -4,31 +4,73 @@
 package Cliente;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
 
 /**
  * @author isaacmontielsanchez
  *
  */
-public class VentanaCliente extends JFrame {
+ class VentanaCliente extends JFrame {
 
 	/**
 	 * @construc constructor dela marco
 	 */
 	public VentanaCliente() {
 		setBounds(1200,300,280,350);
-		JPanel lamina = new JPanel();
-		lamina.setLayout(new BorderLayout());
-		areatexto = new JTextArea();
-		lamina.add(areatexto,BorderLayout.CENTER);
-		add(lamina);
+		LaminaCliente lcliente = new LaminaCliente();
+		add(lcliente);
 		setVisible(true);
 		
 	}
+}
+	 class LaminaCliente extends JPanel{
+		 public LaminaCliente(){
+			 
+		JLabel titulo = new JLabel("Cliente");
+		add(titulo);
+		campo= new JTextField(20);
+		add(campo);
+		btnEnviar = new JButton("Enviar");
+		EnviarTexto eventEnv = new EnviarTexto();
+	btnEnviar.addActionListener(eventEnv);
+		add(btnEnviar);
+		 }
 	
-	private JTextArea areatexto;
+	private JTextField campo;
+	private JButton btnEnviar;
+	
+	private class EnviarTexto implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+//			campo.getText();
+			try {
+				Socket socket1 = new Socket("192.168.1.101" , 9999);
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				System.out.println(e1.getMessage());
+			}
+			
+		}
+		
+	}
 
 }
